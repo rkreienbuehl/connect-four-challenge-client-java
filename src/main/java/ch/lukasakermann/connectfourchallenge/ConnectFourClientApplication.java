93a2527c23f43b728badcd798f8f7c2a0a09ec74
rogerkreienbuehl <r.kreienbuehl>
2019-05-03 14:43:58 +0200

    Set strategy for Alice to LWRStrategy

--- src/main/java/ch/lukasakermann/connectfourchallenge/ConnectFourClientApplication.java
+++ src/main/java/ch/lukasakermann/connectfourchallenge/ConnectFourClientApplication.java
@@ -2,6 +2,7 @@ package ch.lukasakermann.connectfourchallenge;
 
 import ch.lukasakermann.connectfourchallenge.connectFourService.ConnectFourAdapter;
 import ch.lukasakermann.connectfourchallenge.game.GameRunner;
+import ch.lukasakermann.connectfourchallenge.game.strategy.LWRStrategy;
 import ch.lukasakermann.connectfourchallenge.game.strategy.RandomStrategy;
 
 import java.util.concurrent.ExecutorService;
@@ -15,7 +16,7 @@ public class ConnectFourClientApplication {
     public static void main(String[] args) {
         ConnectFourAdapter connectFourAdapter = new ConnectFourAdapter(SERVER_URL);
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.submit(new GameRunner(connectFourAdapter, "Alice", new RandomStrategy(), NUMBER_OF_GAMES));
+        executor.submit(new GameRunner(connectFourAdapter, "Alice", new LWRStrategy(), NUMBER_OF_GAMES));
         executor.submit(new GameRunner(connectFourAdapter, "Bob", new RandomStrategy(), NUMBER_OF_GAMES));
     }
 }
